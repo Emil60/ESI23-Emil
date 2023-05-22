4a616a3ebe369840792304ae02b38db7200e2a81
Emil Ojagverdiyev <emojag@ttu.ee>
2023-05-22 16:33:40 +0300

    task 5

--- adviserservice/src/main/java/com/esi/adviserservice/service/RPLAdvisorService.java
+++ adviserservice/src/main/java/com/esi/adviserservice/service/RPLAdvisorService.java
@@ -1,10 +1,14 @@
 package com.esi.adviserservice.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.esi.adviserservice.dto.RPLRequestDto;
-
+import com.esi.adviserservice.model.RPLAdvisor;
+import com.esi.adviserservice.repository.RPLAdvisorRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,11 +18,23 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class RPLAdvisorService {
  
+    @Autowired
+    private RPLAdvisorRepository rPLAdvisorRepository;
+    
+    private final KafkaTemplate<String, RPLRequestDto> kafkaTemplate;
+    
+    // Task 5
+    
+    @KafkaListener(topics = "studentRequestCreatedTopic", groupId = "requestEventGroup" )
 
+    public void updateRPLResponse(RPLRequestDto rPLRequestDto) {
+        RPLAdvisor rplAdvisor = rPLAdvisorRepository.findById(rPLRequestDto.getUserId()).get();
 
-public void updateRPLResponse(RPLRequestDto rPLRequestDto) {
+        rplAdvisor.setAdvisoryDescription(null);
+        rPLAdvisorRepository.save(rplAdvisor);
 
-}
+    }
+    // Task 5
 
 }
 
